12dcbd9c7de861d9160cc1f8cbc8630aacfd9534
Ricky-OdinEverest <rickyoeverest@gmail.com>
2025-06-09 16:19:35 -0700

    Initial commit of Unreal Engine project

---
+++ Source/CustomCMC/CustomCMC.Build.cs
@@ -0,0 +1,43 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class CustomCMC : ModuleRules
+{
+	public CustomCMC(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"CustomCMC",
+			"CustomCMC/Variant_Platforming",
+			"CustomCMC/Variant_Combat",
+			"CustomCMC/Variant_Combat/AI",
+			"CustomCMC/Variant_SideScrolling",
+			"CustomCMC/Variant_SideScrolling/Gameplay",
+			"CustomCMC/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/CustomCMC/CustomCMC.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CustomCMC.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CustomCMC, "CustomCMC" );
+ 
